bc93315ea95cbc0380d88d1b4811b2e9a7672bcb
noobiland <gillinger8@gmail.com>
2018-06-13 11:32:55 +0300

    adding functionality for iter2

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,27 +1,47 @@
 package com.acme.edu;
 
 public class Logger {
-    static int sum = 0;
+    private static int integerSum = 0;
+
+    public static void logIntBuffer(){
+        output("primitive: " + integerSum);
+        integerSum = 0;
+    }
+
     public static void log(int message) {
-        sum += message;
-       // System.out.println("primitive: " + sum);
-        if (message == 0)
-            System.out.println("primitive: " + message);
+        integerSum += message;
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        logIntBuffer();
+        String decoratedMessage = "primitive: " + message;
+        output(decoratedMessage);
     }
 
-    public static void log (char message) { System.out.println("char: " + message); }
+    private static void output(String decoratedMessage) {
+        System.out.println(decoratedMessage);
+    }
+
+    public static void log (char message) {
+        logIntBuffer();
+        output("char: " + message);
+    }
 
     public static void log (String message) {
-        System.out.println("primitive: " + sum);
-        sum = 0;
-        System.out.println("string: " + message);
+        logIntBuffer();
+        output("string: " + message);
     }
 
-    public static void log (boolean message) { System.out.println("primitive: " + message); }
+    public static void log (boolean message) {
+        logIntBuffer();
+        output("primitive: " + message);
+    }
 
-    public static void log (Object message) { System.out.println("reference: " + "@"); }
+    public static void log (Object message) {
+        logIntBuffer();
+        output("reference: " + "@");
+    }
+
+
+
 }

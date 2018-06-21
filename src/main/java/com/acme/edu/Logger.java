25897e479eb1da00d2116c93a1f477b7ed44c3fd
FirePand4 <gillinger8@gmail.com>
2018-06-22 01:35:36 +0300

    beginning 3d iteration

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -11,7 +11,7 @@ public class Logger {
         System.out.println(decoratedMessage);
     }
 
-    public static void flushIntBuffer(){
+    public static void flushIntBuffer() {
         if (intAccum) {
             output("primitive: " + integerSum);
             integerSum = 0;
@@ -20,18 +20,17 @@ public class Logger {
     }
 
     public static void flushStringBuffer() {
-        if(stringCounter == 1){
+        if (stringCounter == 1) {
             output("string: " + prevState);
-        }
-        else {
-            output("string: " + prevState + " (x"+stringCounter+")");
+        } else {
+            output("string: " + prevState + " (x" + stringCounter + ")");
         }
         stringCounter = 1;
         prevState = null;
     }
 
     public static void log(int message) {
-        if(stringAccum){
+        if (stringAccum) {
             flushStringBuffer();
             stringAccum = false;
         }
@@ -45,43 +44,42 @@ public class Logger {
         intAccum = true;
     }
 
-    public static void log (String message) {
+    public static void log(String message) {
         flushIntBuffer();
 
-        if(prevState == null){
+        if (prevState == null) {
             prevState = message;
-        }
-        else if (prevState.equals(message)){
+        } else if (prevState.equals(message)) {
             stringCounter++;
-        }
-        else{
+        } else {
             flushStringBuffer();
             prevState = message;
         }
         stringAccum = true;
     }
+
     public static void log(byte message) {
         flushIntBuffer();
         String decoratedMessage = "primitive: " + message;
         output(decoratedMessage);
     }
 
-    public static void log (char message) {
+    public static void log(char message) {
         flushIntBuffer();
         output("char: " + message);
     }
 
-    public static void log (boolean message) {
+    public static void log(boolean message) {
         flushIntBuffer();
         output("primitive: " + message);
     }
 
-    public static void log (Object message) {
+    public static void log(Object message) {
         flushIntBuffer();
         output("reference: " + "@");
     }
 
-    public static void log (int[] message){
+    public static void log(int[] message) {
         StringBuilder decoratedArrayMessage = new StringBuilder("primitives array: ");
         arrayOutput(message, decoratedArrayMessage);
     }
@@ -89,30 +87,27 @@ public class Logger {
     private static void arrayOutput(int[] message, StringBuilder decoratedArrayMessage) {
         decoratedArrayMessage.append("{");
 
-        for(int i=0; i<(message.length - 1); i++){
+        for (int i = 0; i < (message.length - 1); i++) {
             decoratedArrayMessage.append(message[i]).append(", ");
         }
 
-        decoratedArrayMessage.append(message[message.length-1]).append("}");
+        decoratedArrayMessage.append(message[message.length - 1]).append("}");
         System.out.println(decoratedArrayMessage);
     }
 
-    public static void log (int[][] message){
-        System.out.println(message.length);
-        //StringBuilder decoratedMatrixMessage = new StringBuilder("primitives matrix: {"+ System.lineSeparator());
+    public static void log(int[][] message) {
+        StringBuilder decoratedMatrixMessage = new StringBuilder("primitives matrix: {" + System.lineSeparator());
 
-       /* for(int[] innerArray: message){
-            for (int data: innerArray){
-                System.out.println(data);
+        for (int[] innerArray : message) {
+            arrayOutput(innerArray, decoratedMatrixMessage);
+            decoratedMatrixMessage.append(System.lineSeparator());
             }
-        }*/
-            //arrayOutput(message[i], decoratedMatrixMessage);
-            //decoratedMatrixMessage.append(System.lineSeparator());
 
+        decoratedMatrixMessage.append("}");
+        System.out.println(decoratedMatrixMessage);
 
-        //decoratedMatrixMessage.append("}");
-        //System.out.println(decoratedMatrixMessage);
     }
+
 }
 
 
